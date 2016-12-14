684ccba059548c2c156ea05ba642b0add79c88af
SannyMichelle <s.speckmeier@goolemail.com>
2016-12-14 09:31:40 +0100

    ausgelieheneBücher gelöscht!

--- src/application/Benutzer.java
+++ src/application/Benutzer.java
@@ -3,7 +3,6 @@ package application;
 public class Benutzer {
 	
 	private String benutzername;
-	private int ausgelieheneBuecher; 
 	private PersonABC person;
 	private String passwort;
 	
@@ -14,7 +13,6 @@ public class Benutzer {
 			this.benutzername=benutzername;
 			this.passwort=passwort;
 			this.person=person;
-			this.ausgelieheneBuecher=ausgelieheneBuecher;
 		}
 	}
 	
@@ -40,20 +38,13 @@ public class Benutzer {
 			this.passwort = passwort;
 		}
 	}
-
-	public int getAusgelieheneBuecher(){
-		return ausgelieheneBuecher;
-	}
-	public void setAusgelieheneBuecher(int ausgehlieheneBuecher){
-		setAusgelieheneBuecher(ausgelieheneBuecher);
-	}
 	
 	public PersonABC getPerson(){
 		return person;
 	}
 	
 	public String toString(){
-		return person.getDetails()+"\nBenutzername:\n"+getBenutzername()+"\nAnzahl ausgeliehene Bücher:\n"+getAusgelieheneBuecher();
+		return person.getDetails()+"\nBenutzername:\n"+getBenutzername();
 	}
 
 }
