e2b0659d160f9d32c9487c7eff8835fa27372694
Robert <email@example.com>
2016-12-15 14:57:49 +0100

    Buchtyp, Exemplar

---
+++ src/application/Buchtyp.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package application;
+
+/**
+ * @author Robert
+ *
+ */
+public class Buchtyp {
+	
+	private String Autor;
+	private String Titel;
+	private int ISBN;
+	
+	public Buchtyp(String Autor, String Titel, int ISBN)
+	{
+		this.Autor = Autor;
+		this.Titel = Titel;
+		this.ISBN = ISBN;
+	}
+
+}
--- src/application/Exemplar.java
+++ src/application/Exemplar.java
@@ -5,18 +5,19 @@ public class Exemplar {
 	private String BuchID;
 	private BuchstatusET status;
 	private String ISBN;
+	private Exemplar buch;
 	
 	public Exemplar (String BuchID, BuchstatusET status, String ISBN)  
 	{
-		BuchID = BuchID;	
-		status = status;
-		ISBN = ISBN;		
+		this.BuchID = BuchID;	
+		this.status = status;
+		this.ISBN = ISBN;		
 		
 	}
 	
-	public void inventarisieren ()
+	public void inventarisieren (String BuchID, BuchstatusET Status, String ISBN)
 	{
-		
+		buch = new Exemplar (BuchID,Status,ISBN);
 	}
 	
 	public void ausleihen()
