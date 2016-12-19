852c080a64e2516ae196fbd3223344e2f2f8a918
Michael Gottinger <Gottinger@example.com>
2016-12-19 21:30:31 +0100

    Anpassung ButtonHandler

--- src/application/ButtonHandler.java
+++ src/application/ButtonHandler.java
@@ -8,7 +8,6 @@ import gui.*;
 import connectionToDatabase.*;
 import java.awt.event.*;
 import java.sql.SQLException;
-
 import javax.swing.*;
 
 /**
@@ -17,15 +16,12 @@ import javax.swing.*;
  */
 
 public class ButtonHandler implements ActionListener {
-	BenutzerAnlegen benutzerAnlegen;
-	String name;
-	String vorname;
-	String benutzername;
-	String passwort;
-	String straße;
-	int postleitzahl;
-	String ort;
-	String hausnummer;
+	private BenutzerAnlegen benutzerAnlegen;
+	private DB_connection con;
+	private String name, vorname, benutzername, passwort, straße, ort, hausnummer;
+	private int postleitzahl, generatedID;
+	private char art;
+	private Benutzer benutzer;
 
 	// create reference to GUI
 	public ButtonHandler(BenutzerAnlegen benutzerAnlegen) {
@@ -40,28 +36,19 @@ public class ButtonHandler implements ActionListener {
 			// Fall Student erstellen:
 			case "ANLEGEN":
 				GUIDaten();
-				DB_connection con = DB_connection.getDbConnection();
+				con = DB_connection.getDbConnection();
 				switch (this.benutzerAnlegen.getBenutzerArt()) {
 				case "Student":
 					int matrikelnummer = this.benutzerAnlegen.getMatrikelnummer();
 					Studiengruppe studiengruppe = this.benutzerAnlegen.getStudiengruppe();
 					Student student = new Student(name, vorname,matrikelnummer, studiengruppe);
+					art = 's';
 					// Wenn Studentenobjekt erfolgreich erstellt, dann in Datenbank sichern
-					// 1. einfügen in Tabelle Person
-					String insertPerson = "INSERT INTO person(Vorname, Name, Art) VALUES ('"+ student.getVorname()+ "','"+ student.getName()+ "','s');";
-					// erhalten der generierten PersonID und Verbuchung
-					int generatedID = con.executequery_autoKey(insertPerson,true);
-					System.out.println("Erstellte PersonID: " + generatedID);
+					ObjektErstellung(student);
 					// 2. einfügen in Tabelle Student
 					String insertStudent = "INSERT INTO student(Matrikelnummer, Studiengruppe, PersonID) VALUES ("+ student.getMatrikelnummer()+ ",'"+ student.getStudiengruppe().toString()+ "',"+ generatedID + ");";
 					boolean studentVerbucht = con.executequery(insertStudent);
 					System.out.println("Student erfolgreich verbucht: "+ studentVerbucht);
-					// 3. einfügen in Tabelle Benutzer
-					Benutzer benutzer = new Benutzer(benutzername, passwort,student);
-					// Wenn Studentenobjekt erfolgreich erstellt, dann inDatenbank sichern
-					String insertBenutzer = "INSERT INTO benutzer(Benutzername, Passwort, PersonID) VALUES ('"+ benutzer.getBenutzername()+ "','"+ benutzer.getPasswort()+ "',"+ generatedID+ ");";
-					boolean benutzerVerbucht = con.executequery(insertBenutzer);
-					System.out.println("Benutzer erfolgreich verbucht: "+ benutzerVerbucht);
 					//4. einfügen in Tabelle Adresse
 					Adresse adresse = new Adresse (straße, hausnummer, postleitzahl, ort);
 					String insertAdresse = "INSERT INTO adresse(AdressID, Straße, Hausnummer, Postleitzahl, Ort) VALUES ('"+generatedID+"','"+adresse.getStraße()+"','"+adresse.getHausnummer()+"','"+adresse.getPostleitzahl()+"','"+adresse.getOrt()+"');";
@@ -69,35 +56,20 @@ public class ButtonHandler implements ActionListener {
 					System.out.println("Adresse erfolgreich verbucht: "+adresseVerbucht);
 					JOptionPane.showMessageDialog(new JFrame(),"Student und Benutzer mit folgenden Daten integriert angelegt:\n\nBenutzername: "+ benutzer.getBenutzername()+ student.toString());
 					benutzerAnlegen.tableview.updateSQLTable(DB_connection.getAllUsers());
-					benutzerAnlegen.setName(null);
-					benutzerAnlegen.setVorname(null);
-					benutzerAnlegen.setBenutzername(null);
-					benutzerAnlegen.setPasswort(null);
-					benutzerAnlegen.setMatrikelnummer(null);
-					benutzerAnlegen.setStudiengruppe(null);
+					EintragLöschen();
 					con.disconnect();
 					break;
 				case "Professor":
 					//Daten aus GUI abziehen und Professorobjekt erstellen
 					String fakultät = this.benutzerAnlegen.getFakultät();
 					Professor professor = new Professor(name, vorname, fakultät);
+					art = 'p';
 					//Wenn Professorenobjekt erfolgreich erstellt, dann in Datenbank sichern
-					con = DB_connection.getDbConnection();
-					//1. einfügen in Tabelle Person
-					insertPerson = "INSERT INTO person(Vorname, Name, Art) VALUES ('"+professor.getVorname()+"','"+professor.getName()+"','p');";
-					//erhalten der generierten PersonID und Verbuchung
-					generatedID = con.executequery_autoKey(insertPerson, true);
-					System.out.println("Erstellte PersonID: "+generatedID);
+					ObjektErstellung(professor);
 					//2. einfügen in Tabelle Professor
 					String insertProfessor = "INSERT INTO professor(Fakultät, PersonID) VALUES ('"+professor.getFakultaet()+"',"+generatedID+");";
 					boolean professorVerbucht = con.executequery(insertProfessor);
 					System.out.println("Professert erfolgreich verbucht: "+professorVerbucht);
-					//3. einfügen in Tabelle Benutzer
-					benutzer = new Benutzer(benutzername, passwort, professor);
-					//Wenn Professorenobjekt erfolgreich erstellt, dann in Datenbank sichern
-					insertBenutzer = "INSERT INTO benutzer(Benutzername, Passwort, PersonID) VALUES ('"+benutzer.getBenutzername()+"','"+benutzer.getPasswort()+"',"+generatedID+");";
-					benutzerVerbucht = con.executequery(insertBenutzer);
-					System.out.println("Benutzer erfolgreich verbucht: "+benutzerVerbucht);
 					//4. einfügen in Tabelle Adresse
 					adresse = new Adresse (straße, hausnummer, postleitzahl, ort);
 					insertAdresse = "INSERT INTO adresse(AdressID, Straße, Hausnummer, Postleitzahl, Ort) VALUES ('"+generatedID+"','"+adresse.getStraße()+"','"+adresse.getHausnummer()+"','"+adresse.getPostleitzahl()+"','"+adresse.getOrt()+"');";
@@ -105,33 +77,18 @@ public class ButtonHandler implements ActionListener {
 					System.out.println("Adresse erfolgreich verbucht: "+adresseVerbucht);
 					JOptionPane.showMessageDialog(new JFrame(), "Professor und Benutzer mit folgenden Daten integriert angelegt:\n\nBenutzername: "+benutzer.getBenutzername()+professor.toString());
 					benutzerAnlegen.tableview.updateSQLTable(DB_connection.getAllUsers());
-					benutzerAnlegen.setName(null);
-					benutzerAnlegen.setVorname(null);
-					benutzerAnlegen.setBenutzername(null);
-					benutzerAnlegen.setPasswort(null);
-					benutzerAnlegen.setFakultät(null);
+					EintragLöschen();
 					con.disconnect();
 					break;
 				case "Personal":
 					//Daten aus GUI abziehen und Personalobjekt erstellen
 					Personal personal = new Personal(name, vorname);
-					//Wenn Personobjekt erfolgreich erstellt, dann in Datenbank sichern 
-					con = DB_connection.getDbConnection();
-					//1. einfügen in Tabelle Person
-					insertPerson = "INSERT INTO person(Vorname, Name, Art) VALUES ('"+personal.getVorname()+"','"+personal.getName()+"','b');";
-					//erhalten der generierten PersonID und Verbuchung
-					generatedID = con.executequery_autoKey(insertPerson, true);
-					System.out.println("Erstellte PersonID: "+generatedID);
+					art = 'b';
+					ObjektErstellung(personal);
 					//2. einfügen in Tabelle Personal
 					String insertPersonal = "INSERT INTO personal(PersonID) VALUES ("+generatedID+");";
 					boolean personalVerbucht = con.executequery(insertPersonal);
 					System.out.println("Personal erfolgreich verbucht: "+personalVerbucht);
-					//3. einfügen in Tabelle Benutzer
-					benutzer = new Benutzer(benutzername, passwort, personal);
-					//Wenn Studentenobjekt erfolgreich erstellt, dann in Datenbank sichern
-					insertBenutzer = "INSERT INTO benutzer(Benutzername, Passwort, PersonID) VALUES ('"+benutzer.getBenutzername()+"','"+benutzer.getPasswort()+"',"+generatedID+");";
-					benutzerVerbucht = con.executequery(insertBenutzer);
-					System.out.println("Benutzer erfolgreich verbucht: "+benutzerVerbucht);
 					//4. einfügen in Tabelle Adresse
 					adresse = new Adresse (straße, hausnummer, postleitzahl, ort);
 					insertAdresse = "INSERT INTO adresse(AdressID, Straße, Hausnummer, Postleitzahl, Ort) VALUES ('"+generatedID+"','"+adresse.getStraße()+"','"+adresse.getHausnummer()+"','"+adresse.getPostleitzahl()+"','"+adresse.getOrt()+"');";
@@ -139,10 +96,7 @@ public class ButtonHandler implements ActionListener {
 					System.out.println("Adresse erfolgreich verbucht: "+adresseVerbucht);
 					JOptionPane.showMessageDialog(new JFrame(), "Personal und Benutzer mit folgenden Daten integriert angelegt:\n\nBenutzername: "+benutzer.getBenutzername()+personal.toString());
 					this.benutzerAnlegen.tableview.updateSQLTable(DB_connection.getAllUsers());
-					benutzerAnlegen.setName(null);
-					benutzerAnlegen.setVorname(null);
-					benutzerAnlegen.setBenutzername(null);
-					benutzerAnlegen.setPasswort(null);
+					EintragLöschen();
 					con.disconnect();
 					break;
 				}
@@ -169,7 +123,7 @@ public class ButtonHandler implements ActionListener {
 		}
 	}
 
-	// befüllt für jeden Benutzer die Grundinformationen TODO ADRESSE?
+	// befüllt für jeden Benutzer die Grundinformationen
 	private void GUIDaten() {
 		name = this.benutzerAnlegen.getName();
 		vorname = this.benutzerAnlegen.getVorname();
@@ -179,5 +133,39 @@ public class ButtonHandler implements ActionListener {
 		hausnummer = this.benutzerAnlegen.getHausnummer();
 		postleitzahl = this.benutzerAnlegen.getPLZ();
 		ort = this.benutzerAnlegen.getOrt();
+	}
+	
+	// befüllt die für jedes Objekt identischen Tabellen
+	private void ObjektErstellung(PersonABC person){
+		try{
+		// 1. einfügen in Tabelle Person
+		String insertPerson = "INSERT INTO person(Vorname, Name, Art) VALUES ('"+ person.getVorname()+ "','"+ person.getName()+ "','"+art+"');";
+			// erhalten der generierten PersonID und Verbuchung
+			generatedID = con.executequery_autoKey(insertPerson, true);
+			System.out.println("Erstellte PersonID: " + generatedID);
+			// 3. einfügen in Tabelle Benutzer
+			benutzer = new Benutzer(benutzername, passwort,person);
+			// Wenn Studentenobjekt erfolgreich erstellt, dann inDatenbank sichern
+			String insertBenutzer = "INSERT INTO benutzer(Benutzername, Passwort, PersonID) VALUES ('"+ benutzer.getBenutzername()+ "','"+ benutzer.getPasswort()+ "',"+ generatedID+ ");";
+			boolean benutzerVerbucht = con.executequery(insertBenutzer);
+			System.out.println("Benutzer erfolgreich verbucht: "+ benutzerVerbucht);
+		} catch (SQLException sqlEx) {
+			JOptionPane.showMessageDialog(new JFrame(), sqlEx.getMessage());
+		}
+	}
+		
+	// entfernt alle Einträge in GUI nach Verbuchung
+	private void EintragLöschen(){
+		benutzerAnlegen.setName(null);
+		benutzerAnlegen.setVorname(null);
+		benutzerAnlegen.setBenutzername(null);
+		benutzerAnlegen.setPasswort(null);
+		benutzerAnlegen.setFakultät(null);
+		benutzerAnlegen.setMatrikelnummer(null);
+		benutzerAnlegen.setStudiengruppe(null);
+		benutzerAnlegen.setStraße(null);
+		benutzerAnlegen.setHausnummer(null);
+		benutzerAnlegen.setOrt(null);
+		benutzerAnlegen.setPLZ(null);
 	}
 }
